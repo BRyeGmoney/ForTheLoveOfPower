ff786950b32835fbe28b3c2f4e5c6c632c99de5b
Bryan Gromadzki <bryangrom@gmail.com>
2015-10-06 11:28:56 -0700

    Networking Lobby

--- Unity Project/ForTheLoveOfPower/Assets/ForTheLoveOfPower/Scripts/MenuScripts/MenuBehaviour.cs
+++ Unity Project/ForTheLoveOfPower/Assets/ForTheLoveOfPower/Scripts/MenuScripts/MenuBehaviour.cs
@@ -6,8 +6,11 @@ public class MenuBehaviour : MonoBehaviour {
 
     public Text mpPlayerText;
 
+    private Animator animator;
+
 	// Use this for initialization
 	void Start () {
+        animator = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -17,10 +20,54 @@ public class MenuBehaviour : MonoBehaviour {
 
     public void LookForMatch()
     {
-        if (mpPlayerText != null)
+        SetTextStatus(TextStatus.SearchingForGame);
+        SetTextVisibility(true);
+    }
+
+    public void ChangeToMultiMenu()
+    {
+        SetTextVisibility(false);
+        animator.SetTrigger("MultiplayerPressed");
+    }
+
+    public void ChangeToMainMenu()
+    {
+        SetTextVisibility(false);
+        animator.SetTrigger("BackPressed");
+    }
+
+    public void CreateRoom()
+    {
+        SetTextStatus(TextStatus.WaitingForPlayer);
+        SetTextVisibility(true);
+    }
+
+    private void SetTextVisibility(bool isVisible)
+    {
+        if (isVisible)
         {
-            mpPlayerText.enabled = true;
+            mpPlayerText.color = new Color(mpPlayerText.color.r, mpPlayerText.color.g, mpPlayerText.color.b, 1f);
+            animator.SetBool("MPSearchStatusFluctuate", true);
         }
+        else
+        {
+            mpPlayerText.color = new Color(mpPlayerText.color.r, mpPlayerText.color.g, mpPlayerText.color.b, 0f);
+            animator.SetBool("MPSearchStatusFluctuate", false);
+        }
+    }
+
+    private void SetTextStatus(TextStatus mpStatus)
+    {
+        if (mpStatus == TextStatus.SearchingForGame)
+            mpPlayerText.text = "Searching For Game";
+        else if (mpStatus == TextStatus.WaitingForPlayer)
+            mpPlayerText.text = "Waiting For Player...";
+        else if (mpStatus == TextStatus.PlayerFound) //player connected - host end
+            mpPlayerText.text = "Player found! Ready";
+        else if (mpStatus == TextStatus.HostFound) //player connected - client end
+            mpPlayerText.text = "Waiting for Host to Start";
+        else
+            mpPlayerText.text = "Error: function not defined";
     }
 
 	public void StartGame() {
@@ -30,4 +77,11 @@ public class MenuBehaviour : MonoBehaviour {
 	public void QuitGame() {
 		Application.Quit ();
 	}
+}
+public enum TextStatus
+{
+    SearchingForGame = 0,
+    WaitingForPlayer = 1,
+    PlayerFound = 2,
+    HostFound = 3,
 }
